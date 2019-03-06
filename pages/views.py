901e635e2970a797974428a85c87a6fe57caf7ae
EugeneZnm <eugenenzioki@gmail.com>
2019-03-06 14:06:06 +0300

    index and about view functions

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('#')
+    return render(request, 'pages/index.html')
+
+def about(request):
+     return render(request, 'pages/about.html') 
