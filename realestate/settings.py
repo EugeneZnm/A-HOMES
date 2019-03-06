bec219b62ef4a9afbe0912afbed9c5c9a59bbb03
EugeneZnm <eugenenzioki@gmail.com>
2019-03-06 14:30:50 +0300

    addition of location of static folder

--- realestate/settings.py
+++ realestate/settings.py
@@ -122,4 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# getting path of static folder
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# setting location of static folder
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'realestate/static')
+]
