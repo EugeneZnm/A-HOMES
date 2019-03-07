2d567f0be1a5e474db583125ceb60470f330f116
EugeneZnm <eugenenzioki@gmail.com>
2019-03-07 13:19:08 +0300

    listings and realtors app

--- realestate/urls.py
+++ realestate/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from pages import urls
 
 urlpatterns = [
-    path('/', include('pages.urls')),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
  
