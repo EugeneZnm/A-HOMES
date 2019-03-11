92f333b013b713021339fbd62b8869bb207647dd
EugeneZnm <eugenenzioki@gmail.com>
2019-03-12 02:02:33 +0300

    adding methods to display links, filter, search and edit content

--- listings/admin.py
+++ listings/admin.py
@@ -4,6 +4,12 @@ from .models import Listing
 #adding listing object properties that are visible on admin dashboard after publishing
 class ListingAdmin(admin.ModelAdmin):
  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+ list_display_links = ('id', 'title')
+ list_filter = ('realtor', 'price')
+ list_editable = ('price', 'is_published')
+ search_fields = ('title', 'description', 'address', 'city', 'state', 'price')
+ list_per_page = 25
+ 
 
 # adding listings model to admin section
 admin.site.register(Listing, ListingAdmin)
