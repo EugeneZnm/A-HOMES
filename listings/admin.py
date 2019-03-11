0157698d8cb2032acb45eb3fab0b0987b58a11e2
EugeneZnm <eugenenzioki@gmail.com>
2019-03-12 01:37:44 +0300

    making listing properties visible on dashboard

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Listing
 
+#adding listing object properties that are visible on admin dashboard after publishing
+class ListingAdmin(admin.ModelAdmin):
+ list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+
 # adding listings model to admin section
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
