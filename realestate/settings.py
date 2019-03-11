b5b2c075dd580e8e9f6b7c356742456787516e48
EugeneZnm <eugenenzioki@gmail.com>
2019-03-11 22:42:33 +0300

    media settings

--- realestate/settings.py
+++ realestate/settings.py
@@ -139,3 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
+
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
