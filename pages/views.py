c27f9d94b982fb8f763275e5888a77dde8962b5f
EugeneZnm <eugenenzioki@gmail.com>
2019-04-01 23:05:16 +0300

    getting realtors and mvp realtor(s)

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    # getting three published listings by date
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings':listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-     return render(request, 'pages/about.html') 
+
+    # Get all Realtors
+    realtor = Realtor.objects.order_by('hire_date')
+
+    # GEt mvp realtor
+    mvp = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp': mvp
+    }
+    return render(request, 'pages/about.html', context) 
