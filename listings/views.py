5bda029f032fb05b00009c4985b2c5f899fa73ef
EugeneZnm <eugenenzioki@gmail.com>
2019-04-06 00:15:23 +0300

    addition of search template

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from .choices import price_choices, bedroom_choices, county_choices
 from .models import Listing
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 
@@ -20,10 +21,17 @@ def listing(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
 
     context = {
-        'listing': listing
+        'listing': listing,
+        
     }
 
     return render (request, 'listings/listing.html',context)   
 
 def search(request):
-    return render (request, 'listings/search.html')
+
+    context ={
+        'county_choices': county_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+    }
+    return render (request, 'listings/search.html', context)
