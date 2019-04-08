cddfcf1315d723e03ce2dce9dc738bede507b942
EugeneZnm <eugenenzioki@gmail.com>
2019-04-08 14:14:18 +0300

    add alert messasge to view functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        messages.error(request, 'Testing Error Message')
+        # User Registration
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # User Login
+        return 
+    else:
+        return render(request, 'accounts/login.html')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
