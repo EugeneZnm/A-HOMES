9753b427396f8b9332ea2beca5fb8dde3a037538
EugeneZnm <eugenenzioki@gmail.com>
2019-04-08 13:59:39 +0300

    activating messages app

--- realestate/settings.py
+++ realestate/settings.py
@@ -147,3 +147,10 @@ STATICFILES_DIRS = [
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: '',
+    50: 'critical',
+}
