8410c4180b078eab427a978ad6a3115aa9162ad4
EugeneZnm <eugenenzioki@gmail.com>
2019-04-08 17:06:16 +0300

    logout implementation

--- accounts/views.py
+++ accounts/views.py
@@ -44,8 +44,18 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # User Login
-        return 
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request,'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
@@ -53,4 +63,7 @@ def dashboard(request):
     return render(request, 'accounts/dashboard.html')
 
 def logout(request):
-    return redirect('index')    
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')    
