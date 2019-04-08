e0e6988c55261d4b963d5f5f5234189df0b2ef4f
EugeneZnm <eugenenzioki@gmail.com>
2019-04-08 14:31:25 +0300

    timeout function to automatically clear error message

--- realestate/static/js/main.js
+++ realestate/static/js/main.js
@@ -1,2 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+// timeout function to remove error message automatically
+setTimout(function(){
+    $('#message').fadeOut('slow');
+}, 3000);
