7c51cd9586fa83053eb823e45fa827646ca65385
EugeneZnm <eugenenzioki@gmail.com>
2019-04-11 22:40:27 +0300

    procile, runtime and requirements

--- realestate/settings.py
+++ realestate/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'realestate.urls'
@@ -141,6 +142,7 @@ USE_TZ = True
 # getting path of static folder
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # setting location of static folder
 STATICFILES_DIRS = [
